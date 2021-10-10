f6a05783415fa302963ca37b05da494b3c1df459
Dmitrii Maletskii <mltsk94@gmail.com>
2021-10-10 15:35:56 +0300

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className='header'>
+      <img alt="logo" width='90' height='50' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7xE8fEA6b9OuImCsqbVXW-YcwBKPAnZ7Epw&usqp=CAU" />
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+  <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2159024400&v=beta&t=4WV9YKR9L3PAEnppWmPPMk5xVnETtWvhZN8NexEzPwM" />
+  <div>
+    <img width='200' height='200' src='https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg' />
+    <div>description</div>
+  </div>
+  <div>My posts</div>
+  <div>New posts</div>
+  <div>
+    <div>post 1</div>
+    <div>post 2</div>
+  </div>
+</div>
+}
+
+export default Profile;
