00ae3928be5c90b2d9e0eb5023b866611c5fb69b
Dmitrii Maletskii <mltsk94@gmail.com>
2021-10-11 20:04:53 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Header from './components/Header/Header.jsx'
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        <Profile />
-        {/* <Dialogs /> */}
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,8 @@ import classes from  './Navbar.module.css';
 const Navbar = () => {
   return <nav className={classes.nav}>
   <ul>
-    <li className={`${classes.item} ${classes.active}`}><a href="#">Profile</a></li>
-    <li className={classes.item}><a href="#">Messages</a></li>
+    <li className={`${classes.item} ${classes.active}`}><a href="/profile">Profile</a></li>
+    <li className={classes.item}><a href="/dialogs">Messages</a></li>
     <li className={classes.item}><a href="#">News</a></li>
     <li className={classes.item}><a href="#">Music</a></li>
     <li className={classes.item}><a href="#">Settings</a></li>
