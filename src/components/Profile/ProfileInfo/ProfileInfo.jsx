e0fb04b80a917861bedffed70a43336a43618d98
Dmitrii Maletskii <mltsk94@gmail.com>
2021-11-20 18:41:15 +0300

    add profileStatus

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
+import ProfileStatus from './ProfileStatus';
 
 const ProfileInfo = (props) => {
     if(!props.profile) {
@@ -8,12 +9,12 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <div>
-                <img
-                    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
-            </div>
+            {/* <div>
+                <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
+            </div> */}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
+                <ProfileStatus status="Hello" />
             </div>
         </div>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import s from './ProfileStatus.module.css';
+
+class ProfileStatus extends React.Component {
+    state = {
+        editMode: false,
+    }
+
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    }
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.editMode ||
+                    <div onDoubleClick={ this.activateEditMode }>
+                        {this.props.status}
+                    </div>
+                }
+                {this.state.editMode &&
+                    <div>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status} />
+                    </div>
+                }
+            </div>
+        )
+    }
+}
+
+export default ProfileStatus;
